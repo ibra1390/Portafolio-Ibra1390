3398f2f3bda9318a85e2639c3fae51cba33683b4
ibraim <ing.gonzalezvij@hotmail.com>
2025-10-17 19:17:51 -0600

    ajustes menores a la vista de escritorio de proyectos

--- js/proyectos.js
+++ js/proyectos.js
@@ -30,18 +30,18 @@ function mostrarProyectos() {
   
   proyectos.forEach((proyecto) => {
     proyectosContainer.innerHTML += `
-      <div class="relative w-64 h-80 md:w-80 md:h-96 bg-[url('./images/wanted.png')] bg-contain bg-no-repeat bg-center cursor-pointer transform hover:scale-105 transition-all duration-300 mb-1">
+      <div class="relative w-64 h-80 md:w-80 md:h-96 bg-[url('./images/wanted.png')] bg-contain bg-no-repeat bg-center transform hover:scale-105 transition-all duration-300 mb-1">
         <!-- Imagen del proyecto -->
-        <div class="absolute top-17 md:top-24 left-0 right-0 mx-auto w-46 h-34 md:w-56 md:h-40 overflow-hidden">
-          <img class="w-full h-full object-cover hover:scale-110 transition-transform duration-300" src="${proyecto.imagen}" alt="${proyecto.titulo}" />
+        <div class="absolute top-17 md:top-20 left-0 right-0 mx-auto w-46 h-34 md:w-56 md:h-41 overflow-hidden">
+          <img class="w-full h-full object-cover hover:scale-110" src="${proyecto.imagen}" alt="${proyecto.titulo}" />
         </div>
         
         <!-- Título y descripción -->
-        <div class="absolute top-56 md:top-64 left-0 right-0 mx-auto text-center w-48 md:w-64">
+        <div class="absolute top-56 md:top-68 left-0 right-0 mx-auto text-center w-48 md:w-64">
           <h3 class="text-lg md:text-xl font-bold text-black">${proyecto.titulo}</h3>
           <p class="text-xs md:text-sm text-black font-medium leading-tight">${proyecto.descripcion}</p>
           <a href="${proyecto.enlace}" target="_blank"
-             class="inline-block md:mt-2 md:px-4 text-black text-xs font-bold hover:underline hover:text-red-700">
+             class="inline-block md:mt-2 md:px-4 text-black text-xs font-bold hover:underline hover:text-red-700 cursor-pointer">
             VER PROYECTO
           </a>
         </div>
